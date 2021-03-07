a3d98713e7c3b20d1b20274d4527483f74d94789
emily40830 <emily40830@gmail.com>
2021-03-07 22:59:17 +0800

    fix database path

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -4,6 +4,7 @@ export default async (req, res) => {
   //const result = await users.findAll();
   //console.log('users', models.users);
   let sql = `SELECT * FROM USERS`;
+  console.log('db', db);
   //const result = await db.get(sql);
   db.all(sql, (err, rows) => {
     if (err) res.status(500).json(err);
